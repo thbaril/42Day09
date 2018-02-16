f53200fcad72900a5155a5a4be017f2e1975b31e
kilo <kilowwwww@gmail.com>
2018-02-16 10:30:54 +0100

    free() juste factory ne marche pas
    Tu dois faire un free() au meme endroits que tu fais des mallocs (mais dans l'ordre inverse), pour un tableau tri-dimensionnel ca veut dire trois mallocs donc deux qui sont faits dans une boucle, donc on fait la meme pour free le tout

--- day09/ex06/ft_destroy.c
+++ day09/ex06/ft_destroy.c
@@ -1,6 +1,21 @@
+#include <stdlib.h>
 #include "ft_ultimator.h"
 
-void	ft_destroy(char ***factory);
+void	ft_destroy(char ***factory)
 {
+	int a;
+	int b;
+
+	a = 0;
+	while (factory[a] != NULL)
+	{
+		b = 0;
+		while (factory[a][b] != NULL)
+		{
+			free(factory[a][b]);
+			b++;
+		}
+		free(factory[a++]);
+	}
 	free(factory);
 }
